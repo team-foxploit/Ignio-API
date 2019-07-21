277f168de34acad400b60a2d9ec5af6164c98704
luk3Sky <ganindudananja@gmail.com>
2019-07-21 17:36:38 +0530

    Added User Info Service

---
+++ user-info-service/src/test/java/com/foxploit/ignio/userinfoservice/UserInfoServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foxploit.ignio.userinfoservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserInfoServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
