53473254a145893e9d59523d25fb5090d94f6a4b
luk3Sky <ganindudananja@gmail.com>
2019-07-21 17:31:26 +0530

    Added Device Info Service

---
+++ device-info-service/src/main/java/com/foxploit/ignio/deviceinfoservice/DeviceInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foxploit.ignio.deviceinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceInfoServiceApplication.class, args);
+	}
+
+}
