1d12391dcc76866ac907d07c0d360f8fd4c2bc02
luk3Sky <ganindudananja@gmail.com>
2019-07-21 17:16:51 +0530

    Added Device Data Service

---
+++ device-data-service/src/main/java/com/foxploit/ignio/devicedataservice/DeviceDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foxploit.ignio.devicedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceDataServiceApplication.class, args);
+	}
+
+}
