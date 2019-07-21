3b3d4f7e4fab0bcdaba61babb0e7f91867bcfbdd
luk3Sky <ganindudananja@gmail.com>
2019-07-21 17:33:07 +0530

    Configured device-info-service settings

--- device-info-service/src/main/java/com/foxploit/ignio/deviceinfoservice/DeviceInfoServiceApplication.java
+++ device-info-service/src/main/java/com/foxploit/ignio/deviceinfoservice/DeviceInfoServiceApplication.java
@@ -2,8 +2,10 @@ package com.foxploit.ignio.deviceinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeviceInfoServiceApplication {
 
 	public static void main(String[] args) {
