9d76c86d1ebc67a9fcd509629cacc16eb29e432f
luk3Sky <ganindudananja@gmail.com>
2019-08-27 21:24:40 +0530

    Cors enable for device-data-service

--- device-data-service/src/main/java/com/foxploit/ignio/devicedataservice/resources/DeviceDataController.java
+++ device-data-service/src/main/java/com/foxploit/ignio/devicedataservice/resources/DeviceDataController.java
@@ -21,12 +21,14 @@ public class DeviceDataController {
     }
 
     // Get one device data response
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/data/{id}", method = RequestMethod.GET)
     public Optional<DeviceData> getDeviceData(@PathVariable("id") String id) {
         return deviceDataService.getDeviceData(id);
     }
 
     // Get all device data response from a device
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/data/all/{deviceId}", method = RequestMethod.GET)
     public DeviceDataDto getAllDeviceData(@PathVariable String deviceId) {
         List<DeviceData> allDeviceDataList = deviceDataService.getAllDeviceData(deviceId);
@@ -43,12 +45,14 @@ public class DeviceDataController {
     }
 
     // Save device data response
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/data", method = RequestMethod.POST)
     public DeviceData saveDeviceData(@RequestBody DeviceData deviceData) {
         return deviceDataService.saveDeviceData(deviceData);
     }
 
     // Delete device data response
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/data/{id}", method = RequestMethod.DELETE)
     public void deleteDeviceData(@PathVariable String id) {
         deviceDataService.deleteDeviceData(id);
