f49ba8a1707263df1abc0ddb3f83ca722812a7e1
luk3Sky <ganindudananja@gmail.com>
2019-10-13 16:28:53 +0530

    Added InitialMigration for devices

--- device-data-service/src/main/java/com/foxploit/ignio/devicedataservice/config/dbmigrations/InitialSetupMigration.java
+++ device-data-service/src/main/java/com/foxploit/ignio/devicedataservice/config/dbmigrations/InitialSetupMigration.java
@@ -2,6 +2,7 @@ package com.foxploit.ignio.devicedataservice.config.dbmigrations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.foxploit.ignio.devicedataservice.DevicedataserviceApp;
+import com.foxploit.ignio.devicedataservice.domain.Device;
 import com.foxploit.ignio.devicedataservice.domain.SensorData;
 import com.foxploit.ignio.devicedataservice.service.dto.DeviceDataDTO;
 import com.github.mongobee.changeset.ChangeLog;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 
 /**
  * Creates the initial database setup.
@@ -21,7 +23,27 @@ public class InitialSetupMigration {
 
     private static final Logger log = LoggerFactory.getLogger(DevicedataserviceApp.class);
 
-    @ChangeSet(order = "01", author = "initiator", id = "01-addInitialSensorData")
+    @ChangeSet(order = "01", author = "initiator", id = "01-addDevices")
+    public void addDevices(MongoTemplate mongoTemplate) {
+        Device device1 = new Device();
+        Device device2 = new Device();
+        Device device3 = new Device();
+        device1.setDeviceId("NODEIGNIOF101");
+        device2.setDeviceId("NODEIGNIOF102");
+        device3.setDeviceId("NODEIGNIOF103");
+        device1.setOwnerId("ADMIN");
+        device2.setOwnerId("ADMIN");
+        device3.setOwnerId("ADMIN");
+        device1.setCreated(LocalDate.now());
+        device2.setCreated(LocalDate.now());
+        device3.setCreated(LocalDate.now());
+        mongoTemplate.save(device1);
+        mongoTemplate.save(device2);
+        mongoTemplate.save(device3);
+        log.info("Device save successful");
+    }
+
+    @ChangeSet(order = "02", author = "initiator", id = "02-addInitialSensorData")
     public void addInitialSensorData(MongoTemplate mongoTemplate) {
 
         ObjectMapper mapper = new ObjectMapper();
